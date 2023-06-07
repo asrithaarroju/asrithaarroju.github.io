a6f86cae00663e2f3a439e0515a40abf79244b8d
asritha.arroju <81504988+asrithaarroju@users.noreply.github.com>
2023-06-07 18:23:45 +0530

    Made changes to toggle the profile

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 alert("Welcome Asritha's Page!!");
 // consol.log("Wel!!")
+
+var data1 = {image : "Golden_R.jpg",name : "Cooper" , Breed: "Golden Retriever"};
+var data2 = {image : "Poodle.jpg",name : "Cookie" , Breed: "Poodle"};
+var isdata1 = true;
+var displaydata;
+var changepic = function(){
+    if(isdata1){
+        displaydata = data2;
+        isdata1=false;
+    }
+    else{
+        displaydata = data1;
+        isdata1 = true;
+    }
+    document.getElementById("image2").src=displaydata.image;
+    document.getElementById("name2").innerHTML=displaydata.name;
+    document.getElementById("Breed2").innerHTML=displaydata.Breed;
+
+}
