ccd6516b56bcf979dae40d7913cba23808225149
asritha.arroju <81504988+asrithaarroju@users.noreply.github.com>
2023-06-07 19:09:10 +0530

    made changes in function name

--- randomScript.js
+++ randomScript.js
@@ -1,5 +1,5 @@
 var userinfo = {name :"" , image:"", username:""};
-var changemypic = function(){
+var changepic = function(){
     fetch("https://randomuser.me/api").then((response)=>response.json()).then((data)=>{
         userinfo.name = data.results[0].name.first;
         userinfo.image = data.results[0].picture.large;
